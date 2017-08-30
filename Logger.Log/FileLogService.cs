525d615d1f12ff0aec48a0c117764a2e7b245af4
prashant15689 <prashantkumar14428@gmail.com>
2017-08-30 15:47:50 +0530

    Logging in application.

---
+++ Logger.Log/FileLogService.cs
@@ -0,0 +1,68 @@
+﻿using log4net;
+using System;
+using System.IO;
+using Logger.Log;
+
+namespace Logger.Log
+{
+    /// <summary>
+    /// Implement ILogService to log the acivity in file
+    /// </summary>
+    public sealed class FileLogService : ILogService
+    {
+        readonly ILog _logger;
+
+        static FileLogService()
+        {
+            // Gets directory path of the calling application
+            // RelativeSearchPath is null if the executing assembly i.e. calling assembly is a
+            // stand alone exe file. 
+            var log4NetConfigDirectory = AppDomain.CurrentDomain.RelativeSearchPath ?? AppDomain.CurrentDomain.BaseDirectory;
+
+            var log4NetConfigFilePath = Path.Combine(log4NetConfigDirectory, "log4net.config");
+            log4net.Config.XmlConfigurator.ConfigureAndWatch(new FileInfo(log4NetConfigFilePath));
+            log4net.Config.XmlConfigurator.Configure();
+        }
+
+        public FileLogService(Type logClass)
+        {
+            _logger = LogManager.GetLogger(logClass);
+        }
+
+
+        public void Fatal(string errorMessage)
+        {
+            if (_logger.IsFatalEnabled)
+                _logger.Fatal(errorMessage);
+        }
+
+        public void Error(string errorMessage)
+        {
+            if (_logger.IsErrorEnabled)
+                _logger.Error(errorMessage);
+        }
+
+        public void Warn(string message)
+        {
+            if (_logger.IsWarnEnabled)
+                _logger.Warn(message);
+        }
+
+        public void Info(string message)
+        {
+            if (_logger.IsInfoEnabled)
+                _logger.Info(message);
+        }
+
+        public void Debug(string message)
+        {
+            if (_logger.IsDebugEnabled)
+                _logger.Debug(message);
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
---
+++ Logger.Log/ILogService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Logger.Log
+{
+    public interface ILogService : IDisposable
+    {
+        void Fatal(string message);
+        void Error(string message);
+        void Warn(string message);
+        void Info(string message);
+        void Debug(string message);
+    }
+}
